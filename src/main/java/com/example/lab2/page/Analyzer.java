d64ca3457d58ec68a945e2a0c7fb2721b786d1e1
satishusk <sanpavkaz@gmail.com>
2023-03-23 14:11:03 +0400

    Fixed phaser race condition and filling parties by tasks for the place of threads, some refactoring

--- src/main/java/com/example/lab2/page/Analyzer.java
+++ src/main/java/com/example/lab2/page/Analyzer.java
@@ -3,7 +3,6 @@ package com.example.lab2.page;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import javax.net.ssl.SSLHandshakeException;
 import java.io.*;
 import java.net.URL;
 import java.util.*;
@@ -15,14 +14,14 @@ import java.util.stream.Collectors;
 @Service
 @Slf4j
 public class Analyzer {
-  private static final Pattern LINK_PATTERN;
+  private static final Pattern HREF_PATTERN;
   private static final Pattern PROTOCOL_DOMAIN_PATTERN;
   private final ExecutorService executorService;
   private final Phaser phaser;
 
   static {
-    var linkRegex = "href=\"([^\"]+)\"[\\s\\S]*?";
-    LINK_PATTERN = Pattern.compile(linkRegex);
+    var hrefRegex = "href=\"([^\"]+)\"[\\s\\S]*?";
+    HREF_PATTERN = Pattern.compile(hrefRegex);
 
     var protocolDomainRegex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#%?=~_|!:,.;]*[-a-zA-Z0-9+&@#%=~_|]";
     PROTOCOL_DOMAIN_PATTERN = Pattern.compile(protocolDomainRegex);
@@ -38,44 +37,61 @@ public class Analyzer {
   public Map<String, Long> pagesNamePerSize(int depth, String address) {
     var map = new ConcurrentHashMap<String, Long>();
     recursiveAnalyze(depth, address, map);
+    waitWhileAnyExecutorServiceThreadStart();
     phaser.arriveAndAwaitAdvance();
-    executorService.shutdown();
+    log.info("End of analyze");
     return map;
   }
 
+  private void waitWhileAnyExecutorServiceThreadStart() {
+    while(phaser.getRegisteredParties() == 1);
+  }
+
   private void recursiveAnalyze(int depth, String address, Map<String, Long> accumulateMap) {
-    if (depth == 1) return;
+    if (depth == -1) return;
 
     Collection<String> subUrls = new HashSet<>();
     try (var in = new URL(address).openConnection().getInputStream()) {
       depth--;
       subUrls = analyzeStream(in, address, accumulateMap);
-    } catch (BrokenBarrierException | InterruptedException | IOException ex)  {
+    } catch (FileNotFoundException ignored) {
+    } catch (IOException ex)  {
       log.error(ex.getMessage());
     }
+
     asyncHandlingForEach(depth, subUrls, accumulateMap);
+  }
+
+  private void asyncHandlingForEach(int depth, Collection<String> subAddresses, Map<String, Long> accumulateMap) {
+    for (String subAddress : subAddresses) {
+      executorService.execute(() -> {
+        phaser.register();
+        recursiveAnalyze(depth, subAddress, accumulateMap);
+        phaser.arriveAndDeregister();
+      });
+    }
   }
 
   private Collection<String> analyzeStream(
     InputStream stream,
     String addressFrom,
     Map<String, Long> accumulateMap
-  ) throws IOException, BrokenBarrierException, InterruptedException {
+  ) throws IOException {
     BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
 
     String inputLine;
     long countBytes = 0;
-    Collection<String> subUrls = new HashSet<>();
+    Collection<String> subAddresses = new HashSet<>();
     while ((inputLine = reader.readLine()) != null) {
       countBytes += inputLine.getBytes().length;
-      Matcher linkMatcher = LINK_PATTERN.matcher(inputLine);
-      while(linkMatcher.find()) {
-        subUrls.add(linkMatcher.group(1));
+      Matcher hrefMatcher = HREF_PATTERN.matcher(inputLine);
+      while(hrefMatcher.find()) {
+        subAddresses.add(hrefMatcher.group(1));
       }
     }
 
     mapLoggedPut(accumulateMap, addressFrom, countBytes);
-    return requiredNotPresentUrls(subUrls, accumulateMap, addressFrom);
+    return requiredNotPresentUrls(subAddresses, accumulateMap, addressFrom);
   }
 
   private void mapLoggedPut(Map<String, Long> accumulateMap, String key, Long value) {
@@ -87,34 +103,28 @@ public class Analyzer {
     }
   }
 
-  private Collection<String> requiredNotPresentUrls(Collection<String> subUrls, Map<String, Long> accumulateMap, String addressFrom) {
-    return requiredHaveConcreteUrlPrefix(subUrls, addressFrom).stream()
+  private Collection<String> requiredNotPresentUrls(
+    Collection<String> subAddresses,
+    Map<String, Long> accumulateMap,
+    String addressFrom
+  ) {
+    return requiredHaveConcreteUrlPrefix(subAddresses, addressFrom).stream()
       .filter(s -> !accumulateMap.containsKey(s))
       .collect(Collectors.toSet());
   }
 
-  private Collection<String> requiredHaveConcreteUrlPrefix(Collection<String> urls, String urlPrefix) {
+  private Collection<String> requiredHaveConcreteUrlPrefix(Collection<String> subAddresses, String urlPrefix) {
     String prefixPattern = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-    Pattern urlPrefixPattern = Pattern.compile("(?i)\\b" + urlPrefix + "/\\w*");
+    Pattern urlPrefixPattern = Pattern.compile("(?i)\\b" + urlPrefix + "\\w*");
 
     var result = new HashSet<String>();
-    for (String url : urls) {
-      if (!url.matches(prefixPattern)) {
-        result.add(urlPrefix + (url.charAt(0) != '/' ? "/" : "" ) + url);
-      } else if (urlPrefixPattern.matcher(url).find()){
-        result.add(url);
+    for (String subAddress : subAddresses) {
+      if (!subAddress.matches(prefixPattern)) {
+        result.add(urlPrefix + (subAddress.charAt(0) == '/' ? subAddress.substring(1) : subAddress));
+      } else if (urlPrefixPattern.matcher(subAddress).find()){
+        result.add(subAddress);
       }
     }
     return result;
-  }
-
-  private void asyncHandlingForEach(int depth, Collection<String> subUrls, Map<String, Long> accumulateMap) {
-    for (String subUrl : subUrls) {
-      phaser.register();
-      executorService.execute(() -> {
-        recursiveAnalyze(depth, subUrl, accumulateMap);
-        phaser.arriveAndDeregister();
-      });
-    }
   }
 }
