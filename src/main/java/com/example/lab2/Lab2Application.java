776ec958289339202e4938e91aebcad6a6bbabb2
satishusk <sanpavkaz@gmail.com>
2023-03-23 14:12:28 +0400

    Deleted init through context for a web

--- src/main/java/com/example/lab2/Lab2Application.java
+++ src/main/java/com/example/lab2/Lab2Application.java
@@ -1,23 +1,11 @@
 package com.example.lab2;
 
-import com.example.lab2.page.InputProvider;
-import com.example.lab2.page.Analyzer;
-import com.example.lab2.page.Printer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class Lab2Application {
   public static void main(String[] args) {
-    var context = SpringApplication.run(Lab2Application.class, args);
-    var inputProvider = context.getBean(InputProvider.class);
-    var analyzer = context.getBean(Analyzer.class);
-    var printer = context.getBean(Printer.class);
-
-    var input = inputProvider.input();
-    var pagePerSize = analyzer.pagesNamePerSize(input.getDepth(), input.getUri());
-    printer.printSummaryPagePerSize(pagePerSize);
+    SpringApplication.run(Lab2Application.class, args);
   }
 }
